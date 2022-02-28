f653b0c890a28fe8e6b1bb27476ec44853b14ee8
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2022-02-28 17:36:18 -0400

    Drew first car

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -13,6 +13,21 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+    double x = 100;
+    double y = 100;
+    //ofSetColor(this->getColor());
+    // Draw body
+    ofDrawRectangle(x + 0, y + 10, 60, 10);
+    // Draw roof and windshields
+    ofDrawLine(x + 10, y + 10, x + 20, y + 0);
+    ofDrawLine(x + 20, y + 0, x + 40, y + 0);
+    ofDrawLine(x + 40, y + 0, x + 50, y + 10);
+
+    ofSetColor(ofColor(255,255,255));
+    // Draw tires
+    ofDrawCircle(x + 15, y + 25, 5);
+    ofDrawCircle(x + 45, y + 25, 5);
+
 }
 
 //--------------------------------------------------------------
